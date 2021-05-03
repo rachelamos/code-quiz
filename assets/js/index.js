6f7b3f67de412b8e0ecfaaf2fb4479c11f908f11
Rachel Amos <rachelamos35@gmail.com>
2021-05-03 10:59:46 -0500

    question and answer rendering when start button pressed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,25 @@
 let startButton = document.querySelector(".start-button");
 let timerElement = document.querySelector(".timer-count");
-let section = document.querySelector(".timer-box")
+let timerBox = document.querySelector(".timer-box");
+let questionBox = document.querySelector(".question-box");
+let question = document.querySelector(".question");
+let answer = document.querySelector(".answer");
 
 let timer;
 let timerCount = 9;
 
 function startTimer() {
-    section.hidden = false;
+    renderQuestions();
+    timerBox.hidden = false;
     timer = setInterval(function() {
         timerCount--;
         console.log(timerCount);
         timerElement.textContent = timerCount;
     }, 1000);
+}
+
+function renderQuestions() {
+    questionBox.hidden = false;
 }
 
 startButton.addEventListener("click", startTimer);
