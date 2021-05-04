c032043bccecd5d5c3618bf8b2feddda0d147fe2
Rachel Amos <rachelamos35@gmail.com>
2021-05-04 13:32:33 -0500

    answer buttons functioning; questions are iterating through the array and displayed properly

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,12 @@ let timerElement = document.querySelector(".timer-count");
 let timerBox = document.querySelector(".timer-box");
 let questionBox = document.querySelector(".question-box");
 let question = document.querySelector(".question");
-let answer = document.querySelector(".answer");
-let questionIndex = 0;
+let answer = document.querySelector(".answer-box");
+let answer1 = document.querySelector(".answer1");
+let answer2 = document.querySelector(".answer2");
+let answer3 = document.querySelector(".answer3");
+let answer4 = document.querySelector(".answer4");
+let index = 0;
 
 let timer;
 let timerCount = 9;
@@ -13,7 +17,7 @@ let questionsArray = [
     {
         question: "Commonly used data types do NOT include:",
         answers: ["Boolean", "String", "Variable", "Number"],
-        correctAnswer: "Variable"
+        correctAnswer: "Number"
     },
     {
         question: "Which of the following is not one of the main three programming languages of web development?",
@@ -26,12 +30,6 @@ let questionsArray = [
         correctAnswer: '""'
     }
 ];
-// console.log(questionsArray.length);
-// console.log(questionsArray[0].question);
-// console.log(questionsArray[0].answers);
-
-// let questionsArray = ["Commonly used data types do NOT include:", "JavaScript is a ____-programming language.", "Arrays can be used to store:"];
-// let answers = ["Boolean", "String", "String", "Variable", "Number"];
 
 // startTimer function starts the timer when the start button is pressed.
 function startTimer() {
@@ -40,7 +38,7 @@ function startTimer() {
     timerBox.hidden = false;
     timer = setInterval(function () {
         timerCount--;
-        console.log(timerCount);
+        // console.log(timerCount);
         timerElement.textContent = timerCount;
     }, 1000);
 }
@@ -48,21 +46,84 @@ function startTimer() {
 // renders the questions and answers when start button pressed and as each question is answered.
 function renderQuestions() {
     questionBox.hidden = false;
-    // question.textContent = questionsArray[0];
     for (let i = 0; i < questionsArray.length; i++) {
-        question.textContent = questionsArray[questionIndex].question;
-        answer.textContent = questionsArray[i].answers;
+        console.log(questionsArray[index].question)
+        question.textContent = questionsArray[index].question;
+        answer1.textContent = questionsArray[index].answers[0];
+        answer2.textContent = questionsArray[index].answers[1];
+        answer3.textContent = questionsArray[index].answers[2];
+        answer4.textContent = questionsArray[index].answers[3];
     }
-    
-    // answer.textContent = answers[0];
 }
 
-// for (var i = 0; i < questionsArray.length; i++) {
-//     (function renderQuestions(i) {
-//         questionsBox.hidden = false;
-//         question.textContent = questionsArray[i].question;
-//         answer.textContent = questionsArray[i].answers;
-//     })
+answer1.addEventListener("click", function(event) {
+    console.log(event);
+    if (timerCount === 0) {
+        return;
+    }
+    let selectedAnswer = event.target.innerText;
+    if (selectedAnswer === questionsArray[index].correctAnswer) {
+        console.log("Correct!");
+        index += 1;
+        renderQuestions();
+    }   else {
+        console.log("Wrong!");
+        index += 1;
+        renderQuestions();
+    }
+})
+answer2.addEventListener("click", function(event) {
+    console.log(event);
+    if (timerCount === 0) {
+        return;
+    }
+    let selectedAnswer = event.target.innerText;
+    if (selectedAnswer === questionsArray[index].correctAnswer) {
+        console.log("Correct!");
+        index += 1;
+        renderQuestions();
+    }   else {
+        console.log("Wrong!");
+        index += 1;
+        renderQuestions();
+    }
+})
+answer3.addEventListener("click", function(event) {
+    console.log(event);
+    if (timerCount === 0) {
+        return;
+    }
+    let selectedAnswer = event.target.innerText;
+    if (selectedAnswer === questionsArray[index].correctAnswer) {
+        console.log("Correct!");
+        index += 1;
+        renderQuestions();
+    }   else {
+        console.log("Wrong!");
+        index += 1;
+        renderQuestions();
+    }
+})
+answer4.addEventListener("click", function(event) {
+    console.log(event);
+    if (timerCount === 0) {
+        return;
+    }
+    let selectedAnswer = event.target.innerText;
+    if (selectedAnswer === questionsArray[index].correctAnswer) {
+        console.log("Correct!");
+        index += 1;
+        console.log(index);
+        renderQuestions();
+    }   else {
+        console.log("Wrong!");
+        index += 1;
+        renderQuestions();
+    }
+})
+
+// function checkAnswer() {
+
 // }
 
 
